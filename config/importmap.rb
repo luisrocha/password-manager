0f5e40abb85e219c4c1b4c9d0bbcf1ab4335ac8d
Luis Rocha <luis88rocha@gmail.com>
2026-05-27 18:15:27 +0100

    vendor openpgp lib locally

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,6 @@
 pin "application"
 pin "vault_crypto"
-pin "openpgp", to: "https://cdn.jsdelivr.net/npm/openpgp@6.2.2/dist/openpgp.min.mjs"
+pin "openpgp", to: "openpgp.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
